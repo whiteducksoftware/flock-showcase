95faa13680ca0d3338741b512616fc6de7267533
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-04-19 15:16:45 +0200

    temporal

--- 04-advanced-features/08-temporal.py
+++ 04-advanced-features/08-temporal.py
@@ -11,20 +11,28 @@ from flock.workflow.temporal_config import (
 
 
 async def main():
-    workflow_config = TemporalWorkflowConfig(
-        task_queue="flock-test-queue",
-        workflow_execution_timeout=timedelta(minutes=10),
-        default_activity_retry_policy=TemporalRetryPolicyConfig(
-            maximum_attempts=2
+ 
+    # --------------------------------
+    # Create the flock with Temporal config
+    # --------------------------------
+    # Set global Temporal parameters
+    # In this case, we're setting the task queue to "flock-test-queue"
+    # and the workflow execution timeout to 10 minutes
+    # and the default activity retry policy to 2 attempts
+    flock = Flock(
+        enable_temporal=True,
+        temporal_config=TemporalWorkflowConfig(
+            task_queue="flock-test-queue",
+            workflow_execution_timeout=timedelta(minutes=10),
+            default_activity_retry_policy=TemporalRetryPolicyConfig(
+                maximum_attempts=2
+            ),
         ),
     )
 
-    flock = Flock(
-        enable_temporal=True,
-        enable_logging=True,
-        temporal_config=workflow_config,
-    )
-
+    # --------------------------------
+    # Create a normal agent
+    # --------------------------------
     agent = FlockFactory.create_default_agent(
         name="my_presentation_agent",
         input="topic",
@@ -33,35 +41,48 @@ async def main():
 
     flock.add_agent(agent)
 
-    content_agent_retry_policy = TemporalRetryPolicyConfig(
-        maximum_attempts=4,
-        initial_interval=timedelta(seconds=2),
-        non_retryable_error_types=["ValueError"],
-    )
-    content_agent_activity_config = TemporalActivityConfig(
-        start_to_close_timeout=timedelta(minutes=1),
-        retry_policy=content_agent_retry_policy,
-    )
-
+   
+    # --------------------------------
+    # Create a Temporal ready agent
+    # --------------------------------
+    # This agent is ready to be used with Temporal
+    # It has a Temporal activity config that sets the start to close timeout to 1 minute
+    # and the retry policy to 4 attempts
+    # and the non retryable error types to "ValueError"
     content_agent = FlockFactory.create_default_agent(
         name="content_agent",
         input="funny_title, funny_slide_headers",
         output="funny_slide_content",
-        temporal_activity_config=content_agent_activity_config,
+        temporal_activity_config=TemporalActivityConfig(
+            start_to_close_timeout=timedelta(minutes=1),
+            retry_policy=TemporalRetryPolicyConfig(
+                maximum_attempts=4,
+                initial_interval=timedelta(seconds=2),
+                non_retryable_error_types=["ValueError"],
+            ),
+        ),
     )
 
     flock.add_agent(content_agent)
 
+    # --------------------------------
+    # Add the agent to the router
+    # --------------------------------
+    # This is the router that will be used to route the my_presentation_agent to the content agent
     agent.add_component(DefaultRouterConfig(hand_off="content_agent"))
 
     print(
-        f"Starting Flock run on Temporal task queue: {workflow_config.task_queue}"
+        f"Starting Flock run on Temporal task queue: {flock.temporal_config.task_queue}"
     )
 
+    # --------------------------------
+    # Run the flock
+    # --------------------------------
+    # This is the main function that will be used to run the flock
     result = await flock.run_async(
         start_agent="my_presentation_agent",
         input={
-            "topic": "A presentation about apples in which cute kitten faces are carved in"
+            "topic": "A presentation about how good of an idea it is to combine ai agents with temporal.io"
         },
     )
 
