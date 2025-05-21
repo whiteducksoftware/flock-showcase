d60310d488ac502d78b4cbd49a7625d28941b0cb
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-05-21 21:23:59 +0200

    versioning

--- 01-getting-started/03-using-a-tool.py
+++ 01-getting-started/03-using-a-tool.py
@@ -20,7 +20,7 @@ agent = FlockFactory.create_default_agent(
     name="my_agent",
     input="url",
     output="title, headings: list[str],"
-    "entities_and_metadata: list[dict[]],"
+    "entities_and_metadata: list[dict[str, str]],"
     "type:Literal['news', 'blog', 'opinion piece', 'tweet']",
     tools=[web_tools.web_content_as_markdown],
     enable_rich_tables=True,  # Instead of the json output, you can use the rich library to render the output as a table
